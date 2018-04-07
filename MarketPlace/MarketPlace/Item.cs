ad63f42ec5b7d15294beaedea0104bfc061afac5
LAPTOP-TO31IR5G\ramil <ramilxp2009@mail.ru>
2018-04-07 11:58:34 +0300

    Created project. Connect SqLite and Entity FR. Created Item.cs

---
+++ MarketPlace/MarketPlace/Item.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MarketPlace
+{
+    public class Item
+    {
+        private string id;
+        private string description;
+        private decimal price;
+
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
